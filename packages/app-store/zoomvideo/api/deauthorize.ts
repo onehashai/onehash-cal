93b89705136a96b4a21cd0ffe650fd1e28d0036d
Rishabh Pandram <rishabh@onehash.ai>
2024-01-03 15:04:35 +0530

    chat support

--- packages/app-store/zoomvideo/api/deauthorize.ts
+++ packages/app-store/zoomvideo/api/deauthorize.ts
@@ -1,10 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    return res.status(405).end("Method Not Allowed");
-  }
-
   try {
     const requestBody = req.body;
 
