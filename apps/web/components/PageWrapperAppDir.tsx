f4242bace37a6f74d7faed0a25650b4b6b91bdf0
Rishabh Pandram <138438522+rishabh-onehashai@users.noreply.github.com>
2024-02-07 15:55:24 +0530

    Update PageWrapperAppDir.tsx

--- apps/web/components/PageWrapperAppDir.tsx
+++ apps/web/components/PageWrapperAppDir.tsx
@@ -61,19 +61,6 @@ function PageWrapper(props: PageWrapperProps) {
           id="page-status"
           dangerouslySetInnerHTML={{ __html: `window.CalComPageStatus = '${pageStatus}'` }}
         />
-        <Script
-          id="microsoft-clarity-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-                    (function(c,l,a,r,i,t,y){
-                        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY}");
-                    `,
-          }}
-        />
         {getLayout(
           props.requiresLicense ? <LicenseRequired>{props.children}</LicenseRequired> : <>{props.children}</>
         )}
