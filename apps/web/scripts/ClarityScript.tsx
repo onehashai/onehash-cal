1aa338146d10399fa8f35afdbd9b0dfb73c08005
Rishabh Pandram <138438522+rishabh-onehashai@users.noreply.github.com>
2024-02-07 11:25:12 +0530

    Update ClarityScript.tsx

--- apps/web/scripts/ClarityScript.tsx
+++ apps/web/scripts/ClarityScript.tsx
@@ -1,23 +1,21 @@
-import { useEffect } from "react";
+import Script from "next/script";
 
 const ClarityScript = () => {
-  useEffect(() => {
-    // This function will be executed on the client side after the component mounts
-    (function (c, l, a, r, i, t, y) {
-      c[a] =
-        c[a] ||
-        function () {
-          (c[a].q = c[a].q || []).push(arguments);
-        };
-      t = l.createElement(r);
-      t.async = 1;
-      t.src = `https://www.clarity.ms/tag/${i}`;
-      y = l.getElementsByTagName(r)[0];
-      y.parentNode.insertBefore(t, y);
-    })(window, document, "clarity", "script", "kuxrvewmyr");
-  }, []);
-
-  return null; // Since this is just for script execution, the component doesn't render anything
+  return (
+    <Script
+      id="microsoft-clarity-init"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_MICROSOFT_CLARITY}");
+                `,
+      }}
+    />
+  );
 };
 
 export default ClarityScript;
