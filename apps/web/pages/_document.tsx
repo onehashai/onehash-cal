e14a16c5f1c577274fd751f21a3f57e5a33eb46f
Rishabh Pandram <138438522+rishabh-onehashai@users.noreply.github.com>
2024-04-01 19:36:11 +0530

    Update _document.tsx

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -82,6 +82,32 @@ class MyDocument extends Document<Props> {
                 `,
             }}
           />
+          {/* Facebook Pixel Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '606640377220056');
+                  fbq('track', 'PageView');
+                `,
+            }}
+          />
+          <noscript>
+            <img
+              alt="Pixel"
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=606640377220056&ev=PageView&noscript=1"
+            />
+          </noscript>
           <link rel="apple-touch-icon" sizes="180x180" href="/api/logo?type=apple-touch-icon" />
           <link rel="icon" type="image/png" sizes="32x32" href="/api/logo?type=favicon-32" />
           <link rel="icon" type="image/png" sizes="16x16" href="/api/logo?type=favicon-16" />
