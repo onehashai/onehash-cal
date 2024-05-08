9b69c4c1a84632b5fe074d57c9ed001c0cfd43c5
Arjun3492 <arjun.kohli.330@gmail.com>
2024-05-08 11:25:15 +0530

    whatsapp chat on booking page to open in pop up window

--- packages/features/bookings/components/event-meta/WhatsappChat.tsx
+++ packages/features/bookings/components/event-meta/WhatsappChat.tsx
@@ -2,25 +2,36 @@ import classNames from "@calcom/lib/classNames";
 import { useLocale } from "@calcom/lib/hooks/useLocale";
 
 export function WhatsappChat({ whatsappNumber }: { whatsappNumber: string }) {
-  function generateWhatsAppLink(phoneNumber: string): string {
+  const generateWhatsAppLink = (phoneNumber: string): string => {
     const cleanedPhoneNumber = phoneNumber.replace(/\D/g, "");
 
     const whatsappLink = `https://wa.me/send?phone=${cleanedPhoneNumber}`;
 
     return whatsappLink;
-  }
+  };
+  const openWhatsAppChat = () => {
+    // Dimensions and other properties of the popup window
+    const width = 800;
+    const height = 600;
+    const left = (window.innerWidth - width) / 2;
+    const top = (window.innerHeight - height) / 2;
+    const options = `width=${width},height=${height},left=${left},top=${top},resizable,scrollbars=yes,status=1`;
+
+    //Generating the whatsapp link
+    const url = generateWhatsAppLink(whatsappNumber);
+    // Open the popup window with the provided URL and options
+    window.open(url, "_blank", options);
+  };
   const { t } = useLocale();
 
   return (
-    <div className="flex flex-row items-center text-sm font-medium">
+    <div onClick={openWhatsAppChat} className="flex cursor-pointer flex-row items-center text-sm font-medium">
       <img
         src="/app-store/whatsapp/icon.svg"
         className={classNames("me-[10px] h-4 w-4")}
         alt="WhatsApp icon"
       />
-      <a href={generateWhatsAppLink(whatsappNumber)} target="_blank" rel="noopener noreferrer">
-        <p className="line-clamp-1">{t("whatsapp_chat")}</p>
-      </a>
+      <p className="line-clamp-1">{t("whatsapp_chat")}</p>
     </div>
   );
 }
