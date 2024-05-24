aa0d450b61834d85ddec7f4b5c14f45306185cb1
Arjun3492 <arjun.kohli.330@gmail.com>
2024-05-24 13:04:43 +0530

    redirecting to log

--- apps/web/lib/apps/installed/[category]/getServerSideProps.tsx
+++ apps/web/lib/apps/installed/[category]/getServerSideProps.tsx
@@ -15,6 +15,8 @@ export async function getServerSideProps(ctx: GetServerSidePropsContext) {
 
   const returnTo = cookies["return_to"];
 
+  console.log("Redirecting to", returnTo);
+
   if (cookies && returnTo) {
     ctx.res.setHeader("Set-Cookie", "return_to=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT");
     const redirect = {
