7603871b3913c2da7940435754ad4a94e85149b2
Arjun3492 <arjun.kohli.330@gmail.com>
2024-06-19 18:42:54 +0530

    updated db migration to v4.1.9

--- packages/prisma/migrations/20240618123729_user_avatar_revert/migration.sql
+++
@@ -1,3 +0,0 @@
--- Add 'avatar' column to 'users' table
-ALTER TABLE "users"
-ADD COLUMN "avatar" TEXT;
--- packages/prisma/zod/webhook.ts
+++ packages/prisma/zod/webhook.ts
@@ -1,27 +1,29 @@
 import * as z from "zod"
 import * as imports from "../zod-utils"
 import { WebhookTriggerEvents } from "@prisma/client"
-import { CompleteUser, UserModel, CompleteTeam, TeamModel, CompleteEventType, EventTypeModel, CompleteApp, AppModel } from "./index"
+import { CompleteApp, AppModel, CompleteEventType, EventTypeModel, CompleteTeam, TeamModel, CompleteUser, UserModel, CompleteWebhookScheduledTriggers, WebhookScheduledTriggersModel } from "./index"
 
 export const _WebhookModel = z.object({
   id: z.string(),
   userId: z.number().int().nullish(),
-  teamId: z.number().int().nullish(),
-  eventTypeId: z.number().int().nullish(),
   subscriberUrl: z.string().url(),
-  payloadTemplate: z.string().nullish(),
   createdAt: z.date(),
   active: z.boolean(),
   eventTriggers: z.nativeEnum(WebhookTriggerEvents).array(),
+  payloadTemplate: z.string().nullish(),
+  eventTypeId: z.number().int().nullish(),
   appId: z.string().nullish(),
   secret: z.string().nullish(),
+  teamId: z.number().int().nullish(),
+  platform: z.boolean(),
 })
 
 export interface CompleteWebhook extends z.infer<typeof _WebhookModel> {
-  user?: CompleteUser | null
-  team?: CompleteTeam | null
-  eventType?: CompleteEventType | null
   app?: CompleteApp | null
+  eventType?: CompleteEventType | null
+  team?: CompleteTeam | null
+  user?: CompleteUser | null
+  scheduledTriggers: CompleteWebhookScheduledTriggers[]
 }
 
 /**
@@ -30,8 +32,9 @@ export interface CompleteWebhook extends z.infer<typeof _WebhookModel> {
  * NOTE: Lazy required in case of potential circular dependencies within schema
  */
 export const WebhookModel: z.ZodSchema<CompleteWebhook> = z.lazy(() => _WebhookModel.extend({
-  user: UserModel.nullish(),
-  team: TeamModel.nullish(),
-  eventType: EventTypeModel.nullish(),
   app: AppModel.nullish(),
+  eventType: EventTypeModel.nullish(),
+  team: TeamModel.nullish(),
+  user: UserModel.nullish(),
+  scheduledTriggers: WebhookScheduledTriggersModel.array(),
 }))
