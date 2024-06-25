379ebe613ef8340a34f258bf688fa97ce12d7291
Arjun3492 <arjun.kohli.330@gmail.com>
2024-06-25 11:33:29 +0530

    upgraded to v4.1.9

--- packages/prisma/migrations/20240618131233_schema_revert/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable.sql
-
--- Add 'avatar' column to 'users' table if it does not exist
-ALTER TABLE "users"
-ADD COLUMN IF NOT EXISTS "avatar" TEXT,
-ADD COLUMN IF NOT EXISTS "away" BOOLEAN NOT NULL DEFAULT false;
-
-
-
