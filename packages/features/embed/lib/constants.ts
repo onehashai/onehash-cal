3b9aeb6ce10007a8ae7f104d4fe1c0e2a28d465a
Arjun3492 <arjun.kohli.330@gmail.com>
2024-08-21 15:32:23 +0530

    fix:embed event types

--- packages/features/embed/lib/constants.ts
+++ packages/features/embed/lib/constants.ts
@@ -1,4 +1,6 @@
-import { EMBED_LIB_URL, WEBAPP_URL } from "@calcom/lib/constants";
+import { WEBAPP_URL } from "../../../lib/constants";
 
-export const embedLibUrl = EMBED_LIB_URL;
-export const EMBED_PREVIEW_HTML_URL = `${WEBAPP_URL}/embed/preview.html`;
+// const WEBAPP_URL = "https://app.cal.com";
+export const embedLibUrl = `${WEBAPP_URL}/embed-link/embed.js`;
+
+export const EMBED_PREVIEW_HTML_URL = `${WEBAPP_URL}/embed-link/preview.html`;
