72a58c6ef73e36476d2d41a4a983696ed17dec1f
Arjun3492 <arjun.kohli.330@gmail.com>
2024-08-29 12:22:36 +0530

    fix:hubspot integration issue

--- packages/lib/apps/getAppOnboardingRedirectUrl.ts
+++ packages/lib/apps/getAppOnboardingRedirectUrl.ts
@@ -4,7 +4,5 @@ import { getAppOnboardingUrl } from "./getAppOnboardingUrl";
 
 export const getAppOnboardingRedirectUrl = async (slug: string, teamId?: number) => {
   const url = await getAppOnboardingUrl({ slug, teamId, step: AppOnboardingSteps.EVENT_TYPES_STEP });
-
-  router.push(url);
   return encodeURIComponent(url);
 };
