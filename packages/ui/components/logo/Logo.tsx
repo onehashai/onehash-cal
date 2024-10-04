6d8595f158d483c83a6195919dcd276e45519885
Arjun3492 <arjun.kohli.330@gmail.com>
2024-10-04 17:06:14 +0530

    fix:logo fix on mobile and tablet view

--- packages/ui/components/logo/Logo.tsx
+++ packages/ui/components/logo/Logo.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import classNames from "@calcom/lib/classNames";
 
 export default function Logo({
@@ -15,24 +13,15 @@ export default function Logo({
   className?: string;
   src?: string;
 }) {
-  const imageSize = small ? { width: 64, height: 64 } : { width: 20, height: 20 };
-
   return (
     <h3 className={classNames("logo", inline && "inline", className)}>
       <strong>
         {icon ? (
           <div className="mx-auto dark:invert">
-            <Image alt="Cal" title="Cal" src={`${src}?type=icon`} width={36} height={36} />
+            <img alt="Cal" title="Cal.id" src={`${src}?type=icon`} />
           </div>
         ) : (
-          <Image
-            className={classNames("dark:invert")}
-            alt="Cal"
-            title="Cal"
-            src={src}
-            width={imageSize.width}
-            height={imageSize.height}
-          />
+          <img alt="Cal.id" title="Cal.id" src={src} className="h-full w-24" />
         )}
       </strong>
     </h3>
