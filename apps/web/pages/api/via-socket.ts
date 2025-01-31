edbd3de1e1a29f9321f52b74f819057cd3a03c63
Arjun3492 <arjun.kohli.330@gmail.com>
2025-01-31 16:20:39 +0530

    fix:session cookie

--- apps/web/pages/api/via-socket.ts
+++ apps/web/pages/api/via-socket.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
 
-import { WEBAPP_URL } from "@calcom/lib/constants";
+import { WEBAPP_URL, IS_PRODUCTION_BUILD } from "@calcom/lib/constants";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   try {
@@ -11,27 +11,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!nextAuthSecret || !viaSocketSecret) {
       return res.status(500).json({ message: "Server error" });
     }
-    const nextAuthToken = await getToken({ req: req, secret: nextAuthSecret });
 
-    const sessionObject = {
-      "next-auth.session-token": req.cookies["next-auth.session-token"],
-    };
-    const sessionObjectStr = Object.entries(sessionObject)
-      .map(([key, value]) => `${key}=${value}`)
-      .join("&");
-    console.log("sessionObjectStr__", sessionObjectStr);
+    const cookieKey = IS_PRODUCTION_BUILD ? "__Secure-next-auth.session-token" : "next-auth.session-token";
+    const sessionToken = req.cookies[cookieKey];
+    if (!sessionToken) {
+      return res.status(400).json({ message: "Session token not found" });
+    }
+    const sessionCookieStr = `${cookieKey}=${sessionToken}`;
+
+    const sessionPayload = await getToken({ req: req, secret: nextAuthSecret });
+    if (!sessionPayload) {
+      return res.status(400).json({ message: "Session not found" });
+    }
     const embedTokenPayload = {
       org_id: "12498",
       chatbot_id: "6788a3851adb5733fc774e2a",
-      user_id: nextAuthToken?.id,
+      user_id: sessionPayload?.id,
       variables: {
-        cookie: sessionObjectStr,
+        cookie: sessionCookieStr,
         origin: WEBAPP_URL,
       },
     };
     const embedToken = jwt.sign(embedTokenPayload, viaSocketSecret);
 
-    return res.status(200).json({ embedToken, userId: nextAuthToken?.id });
+    return res.status(200).json({ embedToken, userId: sessionPayload?.id });
   } catch (err) {
     return res.status(500).json({ message: "Server error" });
   }
