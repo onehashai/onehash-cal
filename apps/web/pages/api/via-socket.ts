d9ac3ec57226ca52638fbf0acb49877b08bc1b54
Arjun3492 <arjun.kohli.330@gmail.com>
2025-01-31 14:44:37 +0530

    session implementation changes

--- apps/web/pages/api/via-socket.ts
+++ apps/web/pages/api/via-socket.ts
@@ -2,6 +2,8 @@ import jwt from "jsonwebtoken";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
 
+import { WEBAPP_URL } from "@calcom/lib/constants";
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   try {
     const nextAuthSecret = process.env.NEXTAUTH_SECRET;
@@ -10,11 +12,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(500).json({ message: "Server error" });
     }
     const nextAuthToken = await getToken({ req: req, secret: nextAuthSecret });
+
+    const sessionObject = {
+      "next-auth.session-token": req.cookies["next-auth.session-token"],
+    };
+    const sessionObjectStr = Object.entries(sessionObject)
+      .map(([key, value]) => `${key}=${value}`)
+      .join("&");
+    console.log("sessionObjectStr__", sessionObjectStr);
     const embedTokenPayload = {
       org_id: "12498",
       chatbot_id: "6788a3851adb5733fc774e2a",
       user_id: nextAuthToken?.id,
-      variables: { cookie: req.cookies },
+      variables: {
+        cookie: sessionObjectStr,
+        origin: WEBAPP_URL,
+      },
     };
     const embedToken = jwt.sign(embedTokenPayload, viaSocketSecret);
 
