c6c919c1b11b5c72a33b2d343326d34ba0534be1
arjun3492 <arjun.kohli.330@gmail.com>
2025-04-15 14:35:18 +0530

    test:build issue

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -179,6 +179,7 @@ const nextConfig = {
     optimizePackageImports: ["@calcom/ui"],
     instrumentationHook: true,
     serverActions: true,
+    outputFileTracingIgnores: ["**/.next/cache/**"],
   },
   i18n: {
     ...i18n,
