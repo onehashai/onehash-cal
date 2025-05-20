0877e041fe8c0907f0d0a5700d48f3f459a2ed5a
arjun3492 <arjun.kohli.330@gmail.com>
2025-05-20 17:58:05 +0530

    chore:configured sentry to catch server level logs

--- packages/lib/logger.ts
+++ packages/lib/logger.ts
@@ -1,7 +1,44 @@
+import * as Sentry from "@sentry/nextjs";
+import type { ILogObj } from "tslog";
 import { Logger } from "tslog";
 
 import { IS_PRODUCTION } from "./constants";
 
+// import { newRelicTransport } from "./newrelic-transporter";
+
+// const stream = fs.createWriteStream("/tmp/nextjs-app.log", { flags: "a" });
+const getLogMessage = (logObject: ILogObj): string => {
+  if (Array.isArray(logObject.args)) {
+    return logObject.args
+      .map((arg) => {
+        if (typeof arg === "object") {
+          return JSON.stringify(arg);
+        }
+        return String(arg);
+      })
+      .join(" ");
+  }
+  return String(logObject.args);
+};
+
+function logLevelToSentry(level: number): Sentry.SeverityLevel {
+  switch (level) {
+    case 0:
+      return "debug";
+    case 1:
+      return "debug";
+    case 2:
+      return "info";
+    case 3:
+      return "warning";
+    case 4:
+      return "error";
+    case 5:
+      return "fatal";
+    default:
+      return "error";
+  }
+}
 const logger = new Logger({
   minLevel: parseInt(process.env.NEXT_PUBLIC_LOGGER_LEVEL || "4"),
   maskValuesOfKeys: ["password", "passwordConfirmation", "credentials", "credential"],
@@ -14,6 +51,25 @@ const logger = new Logger({
     name: "yellow",
     dateIsoStr: "blue",
   },
+  argumentsArrayName: "args",
+  // attachedTransports: [
+  //   (logObject) => {
+  //     // stream.write(`${JSON.stringify(logObject)}\n`);
+  //     newRelicTransport(logObject);
+  //   },
+  // ],
+
+  attachedTransports: [
+    (logObject) => {
+      // Only forward errors to Sentry
+      if (logObject._meta.logLevelId >= 4) {
+        const message = getLogMessage(logObject);
+        Sentry.captureMessage(message, {
+          level: logLevelToSentry(logObject._meta.logLevelId),
+        });
+      }
+    },
+  ],
 });
 
 export default logger;
