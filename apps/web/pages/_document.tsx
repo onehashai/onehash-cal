7740b78f1975fecb428a0024678da2e4517957a1
arjun3492 <arjun.kohli.330@gmail.com>
2025-05-31 15:56:17 +0530

    feat: added customer IO script

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -313,6 +313,55 @@ class MyDocument extends Document<Props> {
               }),
             }}
           />
+
+          {/* Customer IO */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              !function(){
+                var i="cioanalytics", analytics=(window[i]=window[i]||[]);
+                if(!analytics.initialize) {
+                  if(analytics.invoked) {
+                    window.console && console.error && console.error("Snippet included twice.");
+                  } else {
+                    analytics.invoked = !0;
+                    analytics.methods = [
+                      "trackSubmit","trackClick","trackLink","trackForm","pageview","identify",
+                      "reset","group","track","ready","alias","debug","page","once","off","on",
+                      "addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"
+                    ];
+                    analytics.factory = function(e) {
+                      return function() {
+                        var t = Array.prototype.slice.call(arguments);
+                        t.unshift(e);
+                        analytics.push(t);
+                        return analytics;
+                      };
+                    };
+                    for (var e = 0; e < analytics.methods.length; e++) {
+                      var key = analytics.methods[e];
+                      analytics[key] = analytics.factory(key);
+                    }
+                    analytics.load = function(key, e) {
+                      var t = document.createElement("script");
+                      t.type = "text/javascript";
+                      t.async = !0;
+                      t.setAttribute("data-global-customerio-analytics-key", i);
+                      t.src = "https://cdp.customer.io/v1/analytics-js/snippet/" + key + "/analytics.min.js";
+                      var n = document.getElementsByTagName("script")[0];
+                      n.parentNode.insertBefore(t, n);
+                      analytics._writeKey = key;
+                      analytics._loadOptions = e;
+                    };
+                    analytics.SNIPPET_VERSION = "4.15.3";
+                    analytics.load("fa6d11bb6fbfbf91cf0d");
+                    analytics.page();
+                  }
+                }
+              }();
+            `,
+            }}
+          />
         </Head>
 
         <body
