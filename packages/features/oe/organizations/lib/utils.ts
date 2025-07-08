b37e3f37a9355cab584d1b9c1bb9a54e3e28b66c
Rishabh Pandram <138438522+rishabh-onehashai@users.noreply.github.com>
2025-07-08 18:34:17 +0530

    Delete packages/features/oe/organizations directory

--- packages/features/oe/organizations/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export function extractDomainFromEmail(email: string) {
-  let out = "";
-  try {
-    const match = email.match(/^(?:.*?:\/\/)?.*?(?<root>[\w\-]*(?:\.\w{2,}|\.\w{2,}\.\w{2}))(?:[\/?#:]|$)/);
-    out = (match && match.groups?.root) ?? "";
-  } catch (ignore) {}
-  return out.split(".")[0];
-}
